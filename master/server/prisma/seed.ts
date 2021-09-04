06d7b374a8e3dbac91a5d216af5c7c45ef25d559
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2021-09-04 19:39:01 +0300

    подключил postgres и нашел проблему с подключением сидов
    {
      "scripts": {
        "ts-node": "ts-node --compiler-options {\\\"module\\\":\\\"commonjs\\\"}"
      }
    }

--- master/server/prisma/seed.ts
+++ master/server/prisma/seed.ts
@@ -17,3 +17,34 @@ main()
   .finally(async () => {
     await prisma.$disconnect();
   });
+
+
+// import {PrismaClient} from "@prisma/client";
+// import * as dotenv from "dotenv";
+
+// const prisma = new PrismaClient();
+
+// async function main() {
+//   dotenv.config();
+//   console.log("Seeding...");
+
+  
+//    await prisma.product.create({
+//     data: {
+//       name: "SUP Board 14'",
+//       description: "Inflatable SUP Board 14' in all new Blue color.",
+//       price: 629.95,
+//       sku: 'sku_sup_board14_blue',
+//       published: true,
+//     },
+//   });
+// }
+
+// main()
+//   .catch(e => {
+//     console.log(e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
