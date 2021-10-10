deaeadfd47887a84717f1203c5a497df4cbb8948
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2021-10-10 10:27:49 +0300

    amazing smile

--- master/client/components/svg/Smile.tsx
+++ master/client/components/svg/Smile.tsx
@@ -1,11 +1,10 @@
 import { useD3 } from '../hooks/useD3';
 import React from 'react';
 import * as d3 from 'd3';
+import { arc } from 'd3';
+
 
 function Smile() {
-
-
-
 
   const width = 960;
   const height = 500;
@@ -18,30 +17,39 @@ function Smile() {
   const eyeOffSetY = 100;
   const eyeRadius = 40;
 
+  const mouthRadius = 140;
+  const mouthWidth = 20;
+
+
+  const mouthArc = arc().innerRadius(mouthRadius).outerRadius(mouthRadius + mouthWidth).startAngle(Math.PI / 2).endAngle(Math.PI * 3/2)
+
 
   return (
     <svg width={width} height={height}>
+      <g transform={`translate(${centerX}, ${centerY})`}>
       <circle
-        cx={centerX}
-        cy={centerY}
         r="245"
         fill="yellow"
         stroke="black"
         stroke-width={strokeWidth}
       />
       <circle
-        cx={centerX - eyeOffSetX}
-        cy={centerY - eyeOffSetY}
+        cx={-eyeOffSetX}
+        cy={- eyeOffSetY}
         r={eyeRadius}
       />
 
       <circle
-        cx={centerX + eyeOffSetX}
-        cy={centerY - eyeOffSetY}
+        cx={ eyeOffSetX}
+        cy={ - eyeOffSetY}
         r={eyeRadius}
       />
+      <path d={mouthArc()}/>
+      </g>
     </svg>
   );
 }
+
+
 
 export default Smile;
