bcc5652b1184ff3442c743ce95cce81382f81a09
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2021-10-10 11:58:35 +0300

    branches.map working with face

--- master/client/components/svg/Smile.tsx
+++ master/client/components/svg/Smile.tsx
@@ -1,52 +1,37 @@
 import { useD3 } from '../hooks/useD3';
 import React from 'react';
 import * as d3 from 'd3';
-import { arc } from 'd3';
+
+import BackgroundCircle from './circlecomponents/BackgroundCircle';
+import Eyes from './circlecomponents/eyes';
+import Mouth from './circlecomponents/Mouth';
+import FaceContainer from './containers/FaceContainer';
 
 
-function Smile() {
-
-  const width = 960;
-  const height = 500;
-
-  const centerX = width / 2;
-  const centerY = height / 2;
-  const strokeWidth = 10;
-
-  const eyeOffSetX = 90;
-  const eyeOffSetY = 100;
-  const eyeRadius = 40;
-
-  const mouthRadius = 140;
-  const mouthWidth = 20;
 
 
-  const mouthArc = arc().innerRadius(mouthRadius).outerRadius(mouthRadius + mouthWidth).startAngle(Math.PI / 2).endAngle(Math.PI * 3/2)
 
+
+
+function Smile({ 
+  width, 
+  height,
+  centerX,
+  centerY,
+  strokeWidth,
+  eyeOffSetX,
+  eyeOffSetY, 
+  eyeRadius,
+  mouthWidth,
+  mouthRadius
+}) {
 
   return (
-    <svg width={width} height={height}>
-      <g transform={`translate(${centerX}, ${centerY})`}>
-      <circle
-        r="245"
-        fill="yellow"
-        stroke="black"
-        stroke-width={strokeWidth}
-      />
-      <circle
-        cx={-eyeOffSetX}
-        cy={- eyeOffSetY}
-        r={eyeRadius}
-      />
-
-      <circle
-        cx={ eyeOffSetX}
-        cy={ - eyeOffSetY}
-        r={eyeRadius}
-      />
-      <path d={mouthArc()}/>
-      </g>
-    </svg>
+    <FaceContainer width={width} centerX={centerX} centerY={centerY} height={height}>
+      <BackgroundCircle radius={centerY - strokeWidth / 2} strokeWidth={strokeWidth} />
+      <Eyes eyeOffSetX={eyeOffSetX} eyeOffSetY={eyeOffSetY} eyeRadius={eyeRadius} />
+      <Mouth mouthRadius={mouthRadius} mouthWidth={mouthWidth} />
+    </FaceContainer>
   );
 }
 
