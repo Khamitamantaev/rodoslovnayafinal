b8b6b6c6c839729d0c949419e5da39e70660404b
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2021-11-02 13:29:40 +0300

    remove is worjin fine YEAH!~

--- master/server/src/branch/branch.service.ts
+++ master/server/src/branch/branch.service.ts
@@ -40,23 +40,45 @@ export class BranchService {
     return updatedBranch
   }
 
-  async removeBranch(id) {
-    let arr = []
+  async findBranchByID(id: string) {
     const branch = await this.branchModel.findById(id)
-    branch.branches.forEach(async element => {
-      // console.log(element)
-      const branchItem = await this.branchModel.findById(element._id)
-      console.log(branchItem)
-    })
+    return branch
   }
 
+
+  // async deleteChilds(nodeList) {
+
+  //   await Promise.all(nodeList.map(async node=> {
+  //     // console.log(node)
+  //    const branch = await this.branchModel.deleteOne(node)
+
+  //     if(branch.branches) {
+  //       this.deleteChilds(branch.branches)
+  //     }
+  //   }))
+  // }
+
+  async deleteChilds(nodeList) {
+    await this.branchModel.update({}, { $pull: { branches: { _id: nodeList._id } } }, { multi: true })
+    await this.treeModel.update({}, { $pull: { branches: { _id: nodeList._id } } }, { multi: true })
+    if(nodeList.branches) {
+      await Promise.all(nodeList.branches.map( async node => {
+          const branch = await this.branchModel.findOne(node)
+      
+          if(branch.branches) {
+            await this.deleteChilds(branch)
+          }
+          await this.branchModel.deleteOne(node)
+      }))
+    }
+    
+  }
+
+
   async remove(id: string) {
-    // await this.branchModel.update({}, { $pull: { branches: { _id: id } } }, { multi: true })
-    // await this.treeModel.update({}, { $pull: { branches: { _id: id } } }, { multi: true })
-    // const branches = await this.branchModel.find({})
-    await this.removeBranch(id)
-    // console.log(agg)
-    // return this.branchModel.findByIdAndDelete(id)
+      const branch = await this.branchModel.findByIdAndDelete(id)
+      await this.deleteChilds(branch)
+      
   }
 }
 
