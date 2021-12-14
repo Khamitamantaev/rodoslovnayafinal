fc43c4aa21e4f090a165c1b8c0ea652f4db648cb
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2021-12-14 15:10:13 +0300

    add github button login

--- master/client/components/LoginButtons.tsx
+++ master/client/components/LoginButtons.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Flex, Heading, Columns } from 'bumbag';
 import GoogleLogin from 'components/auth/GoogleLogin';
 import useTranslation from 'locales/useTranslation';
+import GitHubLogin from './auth/GitHubLogin';
 
 function LoginButtons() {
   const { t } = useTranslation();
@@ -12,10 +13,10 @@ function LoginButtons() {
         <Flex flexDirection="column">
           <Heading fontSize="400">{t('page.login.heading')}</Heading>
           <br />
-          {/* <GitHubLogin />
+          <GitHubLogin />
+          {/* <br />
+          <RedditLogin /> */}
           <br />
-          <RedditLogin />
-          <br /> */}
           <GoogleLogin />
         </Flex>
       </Columns.Column>
